69ee1cb2f1b583969103cef6c27a699ced292d79
rosandazivanovic <rosa.zivanovic03@gmail.com>
2025-09-20 12:44:32 +0200

    :)

---
+++ Common/ISensorService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface ISensorService
+    {
+        [OperationContract]
+        [FaultContract(typeof(DataFormatFault))]
+        [FaultContract(typeof(ValidationFault))]
+        OperationResult StartSession(SessionMeta meta);
+
+        [OperationContract]
+        [FaultContract(typeof(DataFormatFault))]
+        [FaultContract(typeof(ValidationFault))]
+        OperationResult PushSample(SensorSample sample);
+
+        [OperationContract]
+        OperationResult EndSession(string sessionId);
+
+    }
+}
