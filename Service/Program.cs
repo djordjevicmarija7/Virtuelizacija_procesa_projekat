85c01287fd18cb375672defa77e03ec263148086
rosandazivanovic <rosa.zivanovic03@gmail.com>
2025-09-20 13:08:34 +0200

    :3

--- Service/Program.cs
+++ Service/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace Service
 {
@@ -10,6 +11,18 @@ namespace Service
     {
         static void Main(string[] args)
         {
+            SensorService.OnTransferStarted += id => Console.WriteLine($"[EVENT] Transfer started: {id}");
+            SensorService.OnSampleReceived += (id, sample) => Console.WriteLine($"[EVENT] Sample received: {id} @ {sample.Timestamp:o}");
+            SensorService.OnWarningRaised += (id, msg) => Console.WriteLine($"[WARNING] Session {id}: {msg}");
+            SensorService.OnTransferCompleted += id => Console.WriteLine($"[EVENT] Transfer completed: {id}");
+
+            using (ServiceHost host = new ServiceHost(typeof(SensorService)))
+            {
+                host.Open();
+                Console.WriteLine("SensorService open. Press key to exit.");
+                Console.ReadKey();
+                host.Close();
+            }
         }
     }
 }
